189419eaa35b1d17ae2d240fee13eb167573500f
enriquedaz <enrique83alcocer>
2020-01-24 02:09:40 -0400

    NonAngularPAges

---
+++ 03-NonAngular/alertsDemo.js
@@ -0,0 +1,12 @@
+describe('alerts demo', function () {
+    it('Should works with non angular pages', function () {
+
+        browser.waitForAngularEnabled(false);
+        browser.get('https://www.rahulshettyacademy.com/AutomationPractice/');
+        element(by.id("alertbtn")).click();
+
+       browser.switchTo().alert().accept().then(function () {
+           browser.sleep(5000);
+       })
+    })
+})
